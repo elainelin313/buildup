6522d6945d98ca0636a0e7f6dec8bd720d700959
Elaine Lin <elaine0.tw@gmail.com>
2022-04-16 12:09:38 -0700

    Made homepage following figma design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,7 +1,55 @@
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import BuildOutLogo from '../BuildoutLogo.svg';
+import '../App.css';
+
 function HomePage() {
     return (
       <div className="Home">
-          <h3>Hello World!</h3>
+
+          <div className="nav"> 
+            <div className="buildouttext">
+              <a>BuildOut</a>
+            </div>
+              <div className="navbuttons">
+                login
+              </div>
+              <div className="navbuttons">
+                signup
+              </div>
+          </div>
+
+          <div className="hero">
+
+            <div className="leftcolumn">
+              <img src={BuildOutLogo} alt="BuildOut Logo" />
+            </div>
+
+            <div className="rightcolumn">
+              <h1> workouts curated
+                  <br></br>
+                    just for you </h1>
+                <p>design your perfect workout,
+                  <br></br> 
+                  with hundreds of different 
+                  <br></br>
+                  workout types and equipment
+                </p>
+                <div className="getstartedbutton">
+                  <a>
+                    Get started
+                  </a>
+                </div>
+            </div>
+
+          </div>
+
+          <Button 
+            onClick={() => goToCreateWorkoutPage()}
+            variant="outlined"
+          >
+            Click me to go to CreateWorkoutPage
+          </Button>
       </div>
     );
   }
